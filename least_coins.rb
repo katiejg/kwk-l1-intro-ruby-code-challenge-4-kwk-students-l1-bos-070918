29346d1e667497cc74d2eb2827cc2349833ad0f4
katiejg <jng.katie@gmail.com>
2018-07-22 22:30:42 +0000

    automatically backed up by learn

--- least_coins.rb
+++ least_coins.rb
@@ -1,7 +1,25 @@
 #write out your code here
 
 def least_coins(cents)
-
 #Code your answer here!
-
+coins = { }
+coins[:quarters] = cents / 25
+if cents % 25 <= 0
+  puts coins
+  qv = (cents / 25)
+elsif (cents/25) % 10 <= 0 
+coins[:dimes] = cents % qv / 10
+puts coins
+dv = (cents % qv / 10)
+elsif (cents % (cents/25) / 10) % 5 <= 0
+coins[:nickels] = cents % dv / 5 
+puts coins
+nv = (cents % dv / 5)
+elsif (cents % (cents % (cents/25) / 10) / 5) % 1 <= 0
+pennies = cents % nv / 1
+coins[:pennies] = cents % nv / 1
+puts coins
 end
+end
+
+least_coins(29)
